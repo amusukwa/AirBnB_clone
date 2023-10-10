689dba3931c1bafc92f33b36833c52cca14fe0f2
amusukwa <amusukwa@gmail.com>
2023-10-10 13:23:27 -0700

    filestorage1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,35 @@
+import json
 
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        serialized_objects = {}
+        for key, value in self.__objects.items():
+            serialized_objects[key] = value.to_dict()
+        with open(self.__file_path, "w") as file:
+            json.dump(serialized_objects, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (if it exists)."""
+        try:
+            with open(self.__file_path, "r") as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    cls = models[class_name]
+                    obj = cls(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
