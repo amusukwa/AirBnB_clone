a399cd9cc8c38cc1acf8248b59bda68a9b078686
amusukwa <amusukwa@gmail.com>
2023-10-10 10:25:44 -0700

    task 4

---
+++ test1/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+The BaseModel module
+This class will be the "base" of all other classes in this project
+"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """This class represents the BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialized instance attributes"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the Base Model"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """
+        updates the public instance attribute updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        class_name = self.__class__
+        return {
+            "__class__": self.__class__.__name__,
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
+
