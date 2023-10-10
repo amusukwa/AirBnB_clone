d6e06769b908cdfc415d9e302ecedd4bf31d56e8
Ade-nike17 <perfectnyke@gmail.com>
2023-10-10 13:13:08 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,24 @@ from datetime import datetime
 class BaseModel():
     """This class represents the BaseModel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialized instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns a string representation of the Base Model"""
+        """
+        Returns a string representation of the Base Model
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
@@ -32,10 +42,10 @@ class BaseModel():
         """
         returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        class_name = self.__class__
+        class_name = self.__class__.__name__
         return {
-                "__class__": self.__class__.__name__,
-                "id": self.id,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()
+            "__class__": self.__class__.__name__,
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
         }
