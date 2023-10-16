4fdf5d3fb3e5eca35b328ec4a6666a9d386b9af6
Ade-nike17 <perfectnyke@gmail.com>
2023-10-16 10:42:27 -0700

    unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,35 +1,27 @@
 import unittest
-from datetime import datetime
 from models.base_model import BaseModel
+from datetime import datetime
 
-
-class TestBase(unittest.TestCase):
-
+class TestBaseModel(unittest.TestCase):
     def setUp(self):
-        # Create  instance of the Base class
         self.base_instance = BaseModel()
 
     def test_id_creation(self):
-        # Check if the id attribute is created and is a string
         self.assertTrue(hasattr(self.base_instance, 'id'))
         self.assertIsInstance(self.base_instance.id, str)
 
     def test_created_at_type(self):
-        # Check if created_at is a datetime object
         self.assertIsInstance(self.base_instance.created_at, datetime)
 
     def test_updated_at_type(self):
-        # Check if updated_at is a datetime object
         self.assertIsInstance(self.base_instance.updated_at, datetime)
 
     def test_save_method(self):
-        # Check if the save method updates the updated_at attribute
         original_updated_at = self.base_instance.updated_at
         self.base_instance.save()
         self.assertNotEqual(original_updated_at, self.base_instance.updated_at)
 
     def test_to_dict_method(self):
-        # Check if the to_dict method returns a dictionary with expected keys
         obj_dict = self.base_instance.to_dict()
         self.assertIsInstance(obj_dict, dict)
         self.assertTrue('id' in obj_dict)
