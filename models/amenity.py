3a3f2c89c4bd6a5478d6f5d0f231913b72491c02
Ade-nike17 <perfectnyke@gmail.com>
2023-10-16 01:57:19 -0700

    updates command interpreter

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,4 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """Amenity class that inherits from BaseModel"""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize amenity attributes"""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+    name = ""
